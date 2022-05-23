78f90c7e7ce30805c15adc828a84507bcb8bb02d
dudubecker <eduardobecker182@gmail.com>
2022-05-23 20:11:15 -0300

    Initial commit

---
+++ AA25.py
@@ -0,0 +1,59 @@
+# Importando leitor de instâncias
+from leitor_instancias import Leitor
+
+# Lendo arquivo com as instâncias
+x, y, d, q, e, l, t, n, Cap = Leitor('AA25').dados_instancia
+
+
+# Criando variáveis com valores atualizados a cada inserção
+
+# Pedidos não atendidos
+L = list(range(1,n+1))
+
+# Solução com as rotas -> Inicia-se com uma única rota vazia com os nós do depósito central, para poder iterar desde a primeira iteração
+S = [ [0, 2*n + 1] ]
+
+# Capacidade do veículo na roda
+Cap_atual = 0
+
+# Tempo atual da rota
+Tempo_atual = 0
+
+# Quantidade de requests atendidos
+qtd_atendidos = 0
+
+# Custo total da solução
+FO = 0
+
+# Enquanto todos os pedidos não tiverem sido atendidos
+while qtd_atendidos < n:
+
+    # O número do pedido a ser inserido nas rotas é simplesmente o primeiro daqueles que ainda não foram atendidos
+    request = L[0]
+
+    # Índice do nó de pickup correspondente ao request
+    no_pickup = request
+
+    # Índice do nó de delivery correspondente ao request
+    no_delivery = request + n
+
+    # Para cada rota da solução (rotas criadas paralelamente)
+    for rota in S:
+
+        # considerando que um nó de delivery pode ser no mínimo inserido na segunda (1) posição e no máximo na última
+        for pos_insercao_no_delivery in range(1, len(rota) + 1):
+
+            # considerando que um nó de pickup pode ser no mínimo inserido na primeira (0) posição e no máximo na penúltima
+            for pos_insercao_no_pickup in range(0, len(rota)):
+
+                # Variação da função objetivo pela inserção dos nós x_request e y_request nas posições da iteração
+                delta = (t[rota[pos_insercao_no_pickup]][no_pickup] + t[no_pickup][rota[pos_insercao_no_pickup+1]] - t[rota[pos_insercao_no_pickup]][rota[pos_insercao_no_pickup]+1]) + (t[rota[pos_insercao_no_delivery]][no_delivery] + t[no_delivery][rota[pos_insercao_no_delivery+1]] - t[rota[pos_insercao_no_delivery]][rota[pos_insercao_no_delivery]+1])
+                print(delta)
+
+
+
+
+
+
+
+
---
+++ leitor_instancias.py
@@ -0,0 +1,65 @@
+class Leitor:
+
+    def __init__(self, nome_instancia):
+
+        """Essa classe irá receber como parâmetro o nome do arquivo da instância e retornará,
+           com o método "nome_instancia", os parâmetros de uma instância de PDPTW, na seguinte ordem:
+
+           x -> Coordenada x de cada cliente "i" na instância;
+           y - > Coordenada y de cada cliente "i" na instância;
+           d -> Tempo de serviço de cada cliente "i";
+           q -> Demanda de cada nó "i";
+           e -> Início da janela de tempo de cada nó "i";
+           l -> Fim da janela de tempo de cada nó "i";
+           t -> Tempo de viagem (igual/proporcional à distância entre cada nó);
+           n -> número de pedidos (pares pickup-delivery);
+           Cap -> capacidade de cada veículo
+
+           """
+
+        # Lendo linhas do arquivo
+        lines = open(nome_instancia, 'r').readlines()
+
+        # Computando primeira linha, com parâmetros sem índice da instância
+        first_line = list(map(int, lines[0].split()))
+
+        # Computando demais parâmetros da instância
+        instance = [list(map(float, line.split())) for line in lines[1:]]
+
+        # Coordenada x de cada nó "i"
+        x = [line[1] for line in instance]
+
+        # Coordenada y de cada nó "i"
+        y = [line[2] for line in instance]
+
+        # Tempo de serviço de cada nó "i"
+        d = [line[3] for line in instance]
+
+        # Demanda de cada nó "i"
+        q = [line[4] for line in instance]
+
+        # Início da janela de tempo de cada nó "i"
+        e = [line[5] for line in instance]
+
+        # Fim da janela de tempo de cada nó "i"
+        l = [line[6] for line in instance]
+
+        # Número de requests
+        n = first_line[1]
+
+        # Capacidade de cada veículo
+        Cap = first_line[3]
+
+        # Tempo de viagem (igual/proporcional à distância entre cada nó): inicia-se como 0
+        t = [[0 for i in range(0,n+1)] for j in range(0,n+1)]
+
+        for i in range(0, n+1):
+            for j in range(0, n+1):
+
+                # Calculando distância euclidiana, arrendondando para 2 casas
+                dist = round(((x[i]-x[j])**2 + (y[i]-y[j])**2)**(1/2),2)
+                t[i][j] = dist
+
+
+        # Método que retorna uma tupla com todos os dados da instância, na ordem especificada
+        self.dados_instancia = (x, y, d, q, e, l, t, n, Cap)
